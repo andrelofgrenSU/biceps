0ec8f0c93dab27290971f7fcd5ae669715bb0028
User <andre.lofgren@math.su.se>
2025-02-28 15:23:06 +0100

    small fix

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ mkdir -p $BUILD_DIR
 cd $BUILD_DIR
 export CPATH=$ENV_PATH/include/libxml2
 cmake .. -DCMAKE_PREFIX_PATH=$ENV_PATH -DCMAKE_INSTALL_PREFIX=$ENV_PATH -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
-make preprocess_float_type_header &&  make -j8 && make test_mesh && make test_fem_2d && make test_poisson && make test_stokes && make install
+make -j8 && make test_mesh && make test_fem_2d && make test_poisson && make test_stokes && make install
