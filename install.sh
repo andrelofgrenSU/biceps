afd039c17cb8ed928563b3b0692f3707cbda95e3
André Löfgren <andre.lofgren@math.su.se>
2025-03-14 15:30:20 +0100

    Updated README

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
+NOF_CORES=$(cat /proc/cpuinfo | grep "core id" | sort | uniq | wc -l)
 BUILD_DIR=.build
 mkdir -p $BUILD_DIR
 cd $BUILD_DIR
-cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Release -DENABLE_PYTHON=ON -DENABLE_DOCS=ON -DENABLE_TESTS=ON
-make -j8 && make tests && sudo make install
+cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DENABLE_PYTHON=ON -DENABLE_DOCS=ON -DENABLE_TESTS=OFF
+make -j${NOF_CORES} && make docs && make test && sudo make install
